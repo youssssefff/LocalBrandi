40cdd564f01c9d467f36822cc866822d972eab34
youssssefff <140865979+youssssefff@users.noreply.github.com>
2024-12-13 13:25:39 +0200

    Update login_form.dart

--- frontend/local_brand/lib/widgets/logInWidgets/login_form.dart
+++ frontend/local_brand/lib/widgets/logInWidgets/login_form.dart
@@ -1,76 +1,47 @@
-import 'dart:convert';
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:local_brand/models/user.dart';
-import 'package:local_brand/providers/userProvider.dart';
-import 'package:local_brand/screens/tabs.dart';
-import 'package:local_brand/widgets/global/Buttons/big_button.dart';
-import 'package:local_brand/widgets/global/form_text_field.dart';
-import 'package:http/http.dart' as http;
+import 'package:local_brand/screens/sign_up_screen.dart';
+import 'package:local_brand/widgets/global/TextWidgets/text_w400_f16.dart';
+import 'package:local_brand/widgets/global/TextWidgets/text_w400_f24.dart';
+import 'package:local_brand/widgets/global/secondry_title.dart';
+import 'package:local_brand/widgets/logInWidgets/login_form.dart';
 
-class LoginForm extends ConsumerStatefulWidget {
-  LoginForm({Key? key}) : super(key: key);
-
-  @override
-  ConsumerState<LoginForm> createState() => _LoginFormState();
-}
-
-class _LoginFormState extends ConsumerState<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  final _emailController = TextEditingController();
-
-  final _passwordController = TextEditingController();
-
-  Future<void> submitForm() async {
-    final email = _emailController.text;
-    final password = _passwordController.text;
-    try {
-      final response = await http.post(
-        Uri.parse('http://192.168.1.8:8080/api/users/loginUser'),
-        body: {"email": email, "password": password},
-      );
-      if (response.statusCode == 200) {
-        final userFromBody = response.body;
-        final userMap = json.decode(userFromBody);
-        final user = User.fromJson(userMap);
-        ref.read(userProvider.notifier).addUser(user);
-        Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => Tabs(), // Replace with your widget
-            ));
-      } else {
-        print(response.body);
-      }
-    } catch (error) {
-      print('Error during sign-in: $error');
-    }
-  }
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-      children: [
-        FormTextField(
-          label: "E-Mail",
-          controller: _emailController,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 36, vertical: 40),
+        child: Center(
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+
+              TextW400F24(text: 'Welcome Back',link: false,color: Colors.black,),
+              SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  TextW400F16(text: "or you can ",link: false,color: Color(0xFF767676),),
+                  GestureDetector(
+                      onTap: () =>{Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const SignUpScreen()),
+                      )}, child: TextW400F16(text: "join us!!",link: true,color: Colors.black,))
+                ],
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              LoginForm(),
+              //FormNextButton()
+            ],
+          ),
         ),
-        SizedBox(
-          height: 20,
-        ),
-        FormTextField(
-          label: "Password",
-          controller: _passwordController,
-        ),
-        SizedBox(
-          height: 32,
-        ),
-        BigButton(onPressedFunction: submitForm,color:Colors.black,textColor: Colors.white,title: "Log In",)
-      ],
-    ));
+      ),
+    );
   }
 }
